52e68a71f336c93339bd08ecc1482f93394d8fba
YooHyeok <yjk7454@naver.com>
2024-04-02 11:24:12 +0900

    feat: Repository계층 PackageEntity 추가

---
+++ src/main/java/com/fc/pass/repository/packaze/PackageEntity.java
@@ -0,0 +1,26 @@
+package com.fc.pass.repository.packaze;
+
+import com.fc.pass.repository.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "package")
+public class PackageEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성을 DB에 위임합니다. (AUTO_INCREMENT)
+    private Integer packageSeq;
+
+    private String packageName;
+    private Integer count;
+    private Integer period;
+
+}
