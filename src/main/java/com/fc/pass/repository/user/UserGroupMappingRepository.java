f9e2ed7918521b101c5196fab4e1efac6fb44147
YooHyeok <yjk7454@naver.com>
2024-04-04 00:36:37 +0900

    feat: 관리자 이용권 등록 - UserGroupMapping 도메인 추가 및 id기준 오름차순 리스트 조회

---
+++ src/main/java/com/fc/pass/repository/user/UserGroupMappingRepository.java
@@ -0,0 +1,10 @@
+package com.fc.pass.repository.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UserGroupMappingRepository extends JpaRepository<UserGroupMappingEntity, Integer> {
+    List<UserGroupMappingEntity> findDistinctUserGroupIdByOrderByUserGroupId();
+}
---
+++ src/main/java/com/fc/pass/service/user/UserGroupMappingService.java
@@ -0,0 +1,22 @@
+package com.fc.pass.service.user;
+
+import com.fc.pass.repository.user.UserGroupMappingEntity;
+import com.fc.pass.repository.user.UserGroupMappingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@Service
+public class UserGroupMappingService {
+    private final UserGroupMappingRepository userGroupMappingRepository;
+
+    public UserGroupMappingService(UserGroupMappingRepository userGroupMappingRepository) {
+        this.userGroupMappingRepository = userGroupMappingRepository;
+    }
+
+    public List<String> getAllUserGroupId() {
+        List<UserGroupMappingEntity> userGroupMappingEntities = userGroupMappingRepository.findDistinctUserGroupIdByOrderByUserGroupId();
+        return userGroupMappingEntities.stream().map(UserGroupMappingEntity::getUserGroupId).toList();
+    }
+}
