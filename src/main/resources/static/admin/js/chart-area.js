a2f6a65cc7a85060f6f3e94434dae7d5cb70ce69
webdevyoo <yjk7454@naver.com>
2024-04-05 09:46:23 +0900

    feat: chart-area.js 변수 키워드 var를 let과 const로 적절하게 수정

--- src/main/resources/static/admin/js/chart-area.js
+++ src/main/resources/static/admin/js/chart-area.js
@@ -6,13 +6,13 @@ function number_format(number, decimals, dec_point, thousands_sep) {
     // *     example: number_format(1234.56, 2, ',', ' ');
     // *     return: '1 234,56'
     number = (number + '').replace(',', '').replace(' ', '');
-    var n = !isFinite(+number) ? 0 : +number,
+    let n = !isFinite(+number) ? 0 : +number,
         prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
         sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
         dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
         s = '',
         toFixedFix = function (n, prec) {
-            var k = Math.pow(10, prec);
+            const k = Math.pow(10, prec);
             return '' + Math.round(n * k) / k;
         };
     // Fix for IE parseFloat(0.55).toFixed(0) = 0;
@@ -28,9 +28,9 @@ function number_format(number, decimals, dec_point, thousands_sep) {
 }
 
 // Area Chart Example
-var ctx = document.getElementById("myAreaChart");
+const ctx = document.getElementById("myAreaChart");
 
-var myLineChart = new Chart(ctx, {
+const myLineChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: chartData.labels,
@@ -124,7 +124,7 @@ var myLineChart = new Chart(ctx, {
             caretPadding: 10,
             callbacks: {
                 label: function (tooltipItem, chart) {
-                    var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                    const datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
                     return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
                 }
             }
